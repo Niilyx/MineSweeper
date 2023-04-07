c5f884efcd306f0529564222cc6c49c80feff1d4
Niilyx _ <nait.meddour.idir100@gmail.com>
2023-04-07 08:17:01 +0200

    svelte

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<script>
+  import NewGameButton from './lib/NewGameButton.svelte';
+</script>
+
+<main>
+  <div id="wrapper">
+    <div class="HUD">
+        <NewGameButton />
+    </div>
+    <div id="grid">
+
+    </div>
+  </div>
+</main>
+
+<style>
+  main {
+    width: 100%;
+    height: 100%;
+    margin: 0 auto;
+    background-color: #131516;
+    color: white;
+  }
+  #wrapper {
+    margin: 0 auto;
+    width: 100%;
+    display: flex;
+
+    justify-content: center;
+    align-items: center;
+
+    flex-direction: column;
+  }
+  #grid {
+    margin: 0 auto;
+    width: 80%;
+
+    display: flex;
+    justify-content: center;
+    align-content: center;
+
+    outline: black solid 1px;
+  }
+</style>
---
+++ src/lib/NewGameButton.svelte
@@ -0,0 +1,9 @@
+<script>
+  const NewGameButton = () => {
+    console.log("New Gayme");
+  };
+</script>
+
+<button on:click={NewGameButton}>
+  Nouvelle Partie
+</button>
